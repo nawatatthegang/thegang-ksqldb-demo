514ea8b4d382e7cb981b8cf294e1e97a04f2998f
Nawat Ngerncham <168704945+nawatatthegang@users.noreply.github.com>
2024-09-30 13:39:59 +0700

    data gen with sql scripts instead

---
+++ data_generator/01_add_kessoku_band.sql
@@ -0,0 +1,9 @@
+INSERT INTO bands (name, color, studio)
+VALUES ('Kessoku Band', 'FBF304', 'STARRY');
+
+INSERT INTO characters (name, band_id, band_color, instrument)
+VALUES
+  ('Kita', 2, 'FBF304', 'Vocal&Guitar'),
+  ('Bocchi', 2, 'FBF304', 'Guitar'),
+  ('Ryo', 2, 'FBF304', 'Bass'),
+  ('Nijika', 2, 'FBF304', 'Drum');
--- data_generator/consts.py
+++
@@ -1,1 +0,0 @@
-SOURCE_POSTGRES_URI = "postgresql+psycopg://postgres:postgres@localhost:5432/source_db"
