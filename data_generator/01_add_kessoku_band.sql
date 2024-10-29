3f029324f8934c2c2d3a2c26a3e75aa5ff72b66c
Nawat Ngerncham <168704945+nawatatthegang@users.noreply.github.com>
2024-10-29 16:41:58 +0700

    feat: stream-table join demo

--- data_generator/01_add_kessoku_band.sql
+++
@@ -1,9 +0,0 @@
-INSERT INTO bands (name, color, studio)
-VALUES ('Kessoku Band', 'FBF304', 'STARRY');
-
-INSERT INTO characters (name, band_id, band_color, instrument)
-VALUES
-  ('Kita', 2, 'FBF304', 'Vocal&Guitar'),
-  ('Bocchi', 2, 'FBF304', 'Guitar'),
-  ('Ryo', 2, 'FBF304', 'Bass'),
-  ('Nijika', 2, 'FBF304', 'Drum');
